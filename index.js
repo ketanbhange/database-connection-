769d8ca52ef8bee28bd3b003a54fedb914fcf196
ketan <ketanbhange33@gmail.com>
2024-08-23 15:30:10 +0530

    add files

---
+++ index.js
@@ -0,0 +1,29 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'app',
+  password: 'root',
+});
+try{ connection.query("SHOW TABLES" , (err , result) =>{
+  if(err) throw err;
+  console.log(result);
+});
+}catch(err){
+  console.log(err);
+}
+
+ let createRandomUser = () => {
+    return {
+      userId: faker.string.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      birthdate: faker.date.birthdate(),
+      registeredAt: faker.date.past(),
+    };
+  };
+  console.log(createRandomUser());  
