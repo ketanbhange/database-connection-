7e47db357f96a2bfa5ff641198374a8fe2a3fdc9
ketan <ketanbhange33@gmail.com>
2024-08-23 17:00:33 +0530

    add schemasql

--- index.js
+++ index.js
@@ -7,23 +7,42 @@ const connection = mysql.createConnection({
   database: 'app',
   password: 'root',
 });
-try{ connection.query("SHOW TABLES" , (err , result) =>{
+// let q = "INSERT INTO user (id , username , email , password) VALUES (? , ? , ? , ?)";
+// let user = ["123" , "123_newuser" , "abc@gmail.com" , "abc"];
+
+// if we store the multiple user data;
+
+// let q = "INSERT INTO user (id , username , email , password) VALUES ?";
+// let users = [
+//   ["124" , "123_newuserb" , "abc@gmail.comb" , "abcb"],
+//   ["125" , "123_newuserc" , "abc@gmail.comc" , "abcc"],
+// ]
+
+//insert bulk data using faker
+
+let createRandomUser = () => {
+  return [ 
+     faker.string.uuid(),
+     faker.internet.userName(),
+     faker.internet.email(),
+     faker.internet.password(),
+  ];
+};
+
+let q = "INSERT INTO user (id , username , email , password) VALUES ?";
+let data = [];
+for(let i = 1; i < 100; i++){
+  data.push(createRandomUser());
+}
+
+try{ connection.query(q , [data] , (err , result) =>{
   if(err) throw err;
   console.log(result);
+  console.log(result.length);
 });
 }catch(err){
   console.log(err);
 }
 
- let createRandomUser = () => {
-    return {
-      userId: faker.string.uuid(),
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      avatar: faker.image.avatar(),
-      password: faker.internet.password(),
-      birthdate: faker.date.birthdate(),
-      registeredAt: faker.date.past(),
-    };
-  };
+ 
   console.log(createRandomUser());  
